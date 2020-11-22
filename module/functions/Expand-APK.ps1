e09dc62445bb6809e5c3b7377368230b38de9a45
Гребинаха Анатолій Анатолійович <grebynakha.a@novaposhta.ua>
2020-11-22 16:12:26 +0200

    add new function, add commpess archive password  files
    Compess-APK
    Expand-APK
    
    -not working remove folder ?? Test-pas fals ^( but it exist

---
+++ module/functions/Expand-APK.ps1
@@ -0,0 +1,17 @@
+﻿function Expand-APK() {
+    param (
+       [parameter(Mandatory=$true)]
+       $RootFolder,
+       [parameter(Mandatory=$true)]
+       $IsTest,
+       [parameter(Mandatory=$true)]
+       $APSPass,
+       [parameter(Mandatory=$true)]
+       $OutPath
+    )
+    if (!(Test-Path "C:\Program Files\NP\APS\Temp\$RootFolder")) {
+        [System.Windows.Forms.MessageBox]::Show('Archive not uncompressed or not exist', 'Information', 'OK', 'Error')
+    }
+    Expand-Archive -Path "C:\Program Files\NP\APS\Temp\$RootFolder" -DestinationPath $OutPath
+
+}
